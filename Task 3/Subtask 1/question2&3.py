3c034026c5821e676a74414cad5cdd2c038de48a
mukund0911 <43015972+mukund0911@users.noreply.github.com>
2018-09-05 16:08:16 -0500

    Create question2&3.py

---
+++ Task 3/Subtask 1/question2&3.py
@@ -0,0 +1,17 @@
+import pandas as pd 
+
+train_df = pd.read_csv('train.csv') 
+test_df = pd.read_csv('test.csv')
+combine = train_df.append(test_df) 
+
+datatypes = dict(combine.dtypes)
+categorical_features = []
+numerical_features = []
+
+for i in datatypes:
+    if datatypes[i] in ['object']:
+        categorical_features.append(i)
+    elif datatypes[i] in ['int64', 'float64']:
+        numerical_features.append(i)
+        
+print(numerical_features, categorical_features)
