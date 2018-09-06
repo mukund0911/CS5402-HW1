f4f2b4110cc1b60ef028040fb8f6197b142f13ae
mukund0911 <43015972+mukund0911@users.noreply.github.com>
2018-09-05 21:57:41 -0500

    Create question_9.py

---
+++ Task 3/Subtask 2/question_9.py
@@ -0,0 +1,9 @@
+import pandas as pd 
+import matplotlib.pyplot as plt
+import seaborn as sb
+
+train_df = pd.read_csv('train.csv') 
+test_df = pd.read_csv('test.csv')
+combine = train_df.append(test_df) 
+
+combine[['Survived', 'Pclass']].groupby('Pclass').mean()
