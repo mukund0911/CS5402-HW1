17af834f243fbce0ad67dd907b9441aa887def1d
mukund0911 <43015972+mukund0911@users.noreply.github.com>
2018-09-05 22:01:42 -0500

    Update question5.py

--- Task 3/Subtask 1/question5.py
+++ Task 3/Subtask 1/question5.py
@@ -5,3 +5,8 @@ test_df = pd.read_csv('test.csv')
 combine = train_df.append(test_df) 
 
 combine.columns[combine.isnull().any()].tolist()
+
+"""
+OUTPUT:
+['Age', 'Cabin', 'Embarked', 'Fare', 'Survived']
+"""
