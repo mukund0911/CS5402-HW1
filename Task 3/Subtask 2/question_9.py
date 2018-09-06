7b3861e22fdfc384178280b0bdc20e1995a6bedf
mukund0911 <43015972+mukund0911@users.noreply.github.com>
2018-09-05 22:05:39 -0500

    Update question_9.py

--- Task 3/Subtask 2/question_9.py
+++ Task 3/Subtask 2/question_9.py
@@ -7,3 +7,15 @@ test_df = pd.read_csv('test.csv')
 combine = train_df.append(test_df) 
 
 combine[['Survived', 'Pclass']].groupby('Pclass').mean()
+
+"""
+OUTPUT:
+Survived
+Pclass          
+1       0.629630
+2       0.472826
+3       0.242363
+
+
+AS SIGNIFICANCE OF PCLASS = 1 >0.5, WHICH IS 0.629, IT MUST BE INCLUDED IN THE MODEL.
+"""
