d6ef651b611cc8035de982df2573d8cf25fb53d4
mukund0911 <43015972+mukund0911@users.noreply.github.com>
2018-09-05 22:04:04 -0500

    Update question8.py

--- Task 3/Subtask 1/question8.py
+++ Task 3/Subtask 1/question8.py
@@ -5,3 +5,12 @@ test_df = pd.read_csv('test.csv')
 combine = train_df.append(test_df) 
 
 combine.describe(include=['O'])
+
+"""
+OUTPUT:
+Cabin Embarked                  Name   Sex    Ticket
+count           295     1307                  1309  1309      1309
+unique          186        3                  1307     2       929
+top     C23 C25 C27        S  Connolly, Miss. Kate  male  CA. 2343
+freq              6      914                     2   843        11
+"""
