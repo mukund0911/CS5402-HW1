eba3537821cebc0ebe4daf8420249d3132f70f7a
mukund0911 <43015972+mukund0911@users.noreply.github.com>
2018-09-05 20:00:21 -0500

    Update question_11.py

--- Task 3/Subtask 2/question_11.py
+++ Task 3/Subtask 2/question_11.py
@@ -2,6 +2,10 @@ import pandas as pd
 import matplotlib.pyplot as plt
 import seaborn as sb
 
+train_df = pd.read_csv('train.csv') 
+test_df = pd.read_csv('test.csv')
+combine = train_df.append(test_df) 
+
 g = sb.FacetGrid(combine, col = "Survived")
 g.map(plt.hist, "Age")
 plt.show()
