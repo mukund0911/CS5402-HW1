7d4e4ad203207d0ddca398c0d2460233cbdefbab
mukund0911 <43015972+mukund0911@users.noreply.github.com>
2018-09-05 22:02:34 -0500

    Update question6.py

--- Task 3/Subtask 1/question6.py
+++ Task 3/Subtask 1/question6.py
@@ -6,3 +6,20 @@ combine = train_df.append(test_df)
 
 list_datatypes = combine.dtypes
 print(list_datatypes)
+
+"""
+OUTPUT:
+Age            float64
+Cabin           object
+Embarked        object
+Fare           float64
+Name            object
+Parch            int64
+PassengerId      int64
+Pclass           int64
+Sex             object
+SibSp            int64
+Survived       float64
+Ticket          object
+dtype: object
+"""
